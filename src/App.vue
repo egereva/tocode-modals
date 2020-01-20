d1620f9854e91e753d147d4164f6761369e7cc80
Egereva Marina <marina.egereva@gmail.com>
2020-01-20 11:06:26 +0300

    modals

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <!--first modal-->
+          <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Shoe first button</button>
+          <modals
+            title="First modal"
+            v-show="modalFirst"
+            @close="modalFirst = false">
+            <div slot="body">
+              <p>Text Text Text Text</p>
+              <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Well Done!</button>
+            </div>
+
+          </modals>
+
+          <!--second modal-->
+          <button class="btn btnPrimary" @click="modalSecond.show = !modalSecond.show">Show modal with form</button>
+          <modals
+                  title="Modal with form"
+                  v-show="modalSecond.show"
+                  @close="closeSecondForm">
+            <div slot="body">
+                <form @submit.prevent="submitSecondForm">
+                  <label>Name:</label>
+                  <input type="text" v-model="modalSecond.name" required>
+                  <label>Email:</label>
+                  <input type="email" v-model="modalSecond.email">
+                  <button class="btn btnPrimary">Submit!</button>
+                </form>
+            </div>
+
+          </modals>
+        </div>
+      </section>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import modals from '@/components/Modal.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    modals
+  },
+  data() {
+    return {
+      modalFirst: false,
+      modalSecond: {
+        show: false,
+        name: '',
+        email: ''
+      }
+    }
+  },
+  methods: {
+    submitSecondForm () {
+      console.log({
+        name: this.modalSecond.name,
+        email: this.modalSecond.email
+      })
+      this.modalSecond.name = ''
+      this.modalSecond.email = ''
+      this.modalSecond.show = false
+    },
+    closeSecondForm () {
+      this.modalSecond.name = ''
+      this.modalSecond.email = ''
+      this.modalSecond.show = false
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
+
 </style>
